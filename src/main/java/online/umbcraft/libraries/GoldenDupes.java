2ac5b9a4cffe51a1125f98bef7970337daacf98e
Ivan8or <ivanthegreat825@gmail.com>
2021-04-18 23:11:29 -0400

    commented, cleaned up code

--- src/main/java/online/umbcraft/libraries/GoldenDupes.java
+++ src/main/java/online/umbcraft/libraries/GoldenDupes.java
@@ -20,22 +20,30 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.UUID;
 
-public class GoldenDupes extends JavaPlugin implements Listener {
+public final class GoldenDupes extends JavaPlugin implements Listener {
 
+    // the amount of extra items to be given to the player
     final Map<UUID, Integer> dupeAmnt = new TreeMap<>();
+
+    // the BukkitScheduler task ID of the most recent dupe reset event about to be run
     final Map<UUID, Integer> dupeTask = new TreeMap<>();
+
+    // whether a player used the crafting autocomplete menu, or just clicked / drag clicked the item into the table
     final Map<UUID, Boolean> clickValidity = new TreeMap<>();
 
+
+    // prevents the player from getting an extra item if they touch the crafting table normally
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onClickInv(InventoryClickEvent e) {
+    public void onClickInv(final InventoryClickEvent e) {
         clickValidity.put(e.getWhoClicked().getUniqueId(), false);
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
             clickValidity.remove(e.getWhoClicked().getUniqueId());
         }, 1L);
     }
 
+    // prevents the player from getting an extra item if they touch the crafting table normally
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onDragInv(InventoryDragEvent e) {
+    public void onDragInv(final InventoryDragEvent e) {
 
         clickValidity.put(e.getWhoClicked().getUniqueId(), false);
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
@@ -43,39 +51,54 @@ public class GoldenDupes extends JavaPlugin implements Listener {
         }, 1L);
     }
 
+
+    // increments the amount of duped items a player will receive, and checks to make sure they used the autocomplete
     @EventHandler(priority = EventPriority.HIGH)
-    public void onCraftItem(PrepareItemCraftEvent e) {
+    public void onCraftItem(final PrepareItemCraftEvent e) {
 
         final UUID player_uuid = e.getView().getPlayer().getUniqueId();
+
+
+        // no extra items if the player didn't autocomplete the recipe
         if(clickValidity.containsKey(player_uuid)) {
             return;
         }
 
-        int currentAmnt = dupeAmnt.getOrDefault(player_uuid, 0);
+
+        // increment the number of extra items by 2
+        final int currentAmnt = dupeAmnt.getOrDefault(player_uuid, 0);
         dupeAmnt.put(player_uuid, currentAmnt+2);
 
+
+        // prolongs the time until the extra items reset to 0 by one second
         cancelDupeClearTask(player_uuid);
         dupeTask.put(player_uuid,
                 Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
                 dupeAmnt.remove(player_uuid);
                 }, 20L));
 
+
+        // makes the next click(s) this game tick not give extra items; needed due to how PrepareItemCraftEvent gets triggered
         clickValidity.put(player_uuid, false);
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
             clickValidity.remove(player_uuid);
         }, 0L);
+
     }
 
-    private void cancelDupeClearTask(UUID player) {
-        int lastTask = dupeTask.getOrDefault(player, -1);
+    // cancels an existing queued task which would set the player's extra item count to zero
+    private void cancelDupeClearTask(final UUID player) {
+        final int lastTask = dupeTask.getOrDefault(player, -1);
         if(Bukkit.getScheduler().isQueued(lastTask)) {
             Bukkit.getScheduler().cancelTask(lastTask);
         }
         dupeTask.remove(player);
     }
 
+
+    // player extra item count resets if they leave the menu
     @EventHandler(priority = EventPriority.HIGH)
-    public void onCloseInv(InventoryCloseEvent e) {
+    public void onCloseInv(final InventoryCloseEvent e) {
         dupeAmnt.remove(e.getPlayer().getUniqueId());
         cancelDupeClearTask(e.getPlayer().getUniqueId());
     }
@@ -88,18 +111,21 @@ public class GoldenDupes extends JavaPlugin implements Listener {
         Bukkit.getServer().getPluginManager().registerEvents(this, (Plugin) this);
     }
 
+
     @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerKick(PlayerKickEvent e) {
+    public void onPlayerKick(final PlayerKickEvent e) {
         dupeAmnt.remove(e.getPlayer().getUniqueId());
         dupeTask.remove(e.getPlayer().getUniqueId());
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerLeave(PlayerQuitEvent e) {
+    public void onPlayerLeave(final PlayerQuitEvent e) {
         dupeAmnt.remove(e.getPlayer().getUniqueId());
         dupeTask.remove(e.getPlayer().getUniqueId());
     }
 
+
+    // gives the player the extra items once they pick up after performing the dupe
     @EventHandler(priority = EventPriority.HIGH)
     public void onItemPickup(final EntityPickupItemEvent e) {
 
@@ -108,19 +134,28 @@ public class GoldenDupes extends JavaPlugin implements Listener {
 
         final Player p = (Player) e.getEntity();
 
+
+        // players who didn't do the dupe are not affected
         if (!dupeAmnt.containsKey(p.getUniqueId())) {
             return;
         }
 
-        int dupe_amount = dupeAmnt.get(p.getUniqueId());
+
+        final int dupe_amount = dupeAmnt.get(p.getUniqueId());
         final ItemStack stack = e.getItem().getItemStack();
         stack.setAmount(1);
+
+
+        // maximum # of items gained at one time this way is 64
         final int final_amount = Math.min(64, dupe_amount);
 
+
+        // adding the items one by one to prevent stacking normally-unstackable items like shulkers, etc.
         for(int i = 1; i < final_amount; i++) {
             p.getInventory().addItem(stack);
         }
 
+        // player only dupes the first item he picks up
         dupeAmnt.remove(p.getUniqueId());
     }
 }
