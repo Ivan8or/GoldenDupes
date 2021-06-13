b10e3ca0a9ed0e5df16d56202123214f5ef51026
sprxll <82963474+sprxll@users.noreply.github.com>
2021-06-13 14:16:15 -0300

    Create minecart.java

---
+++ src/main/java/online/umbcraft/libraries/dupes/minecart.java
@@ -0,0 +1,31 @@
+package minecart;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalEnterEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
+
+public class minecartdupe implements Listener
+
+
+
+//detects when the vehicle moves
+
+@EventHandler 
+public static final EntityType MINECART_CHEST;
+
+public VehicleMoveEvent(Vehicle MINECART_CHEST);
+
+//shows when the player opens the minecart inventory (You do it)
+
+//entity enters the portal 
+@EventHandler
+public EntityPortalEnterEvent (Entity StorageMinecart);
+
+//duplicate the inventory (do it yourself)
+
+
+
+}
