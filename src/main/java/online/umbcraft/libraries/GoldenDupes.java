44d8879ef6e3a5e1e4a6ceef5aa8079cb13c7061
Ivan8or <ivanthegreat825@gmail.com>
2021-08-18 10:02:16 -0400

    added bstats for playercount

--- src/main/java/online/umbcraft/libraries/GoldenDupes.java
+++ src/main/java/online/umbcraft/libraries/GoldenDupes.java
@@ -6,7 +6,11 @@ import online.umbcraft.libraries.dupes.AutocraftDupe;
 import online.umbcraft.libraries.dupes.DonkeyDupe;
 import online.umbcraft.libraries.dupes.NetherPortalDupe;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimpleBarChart;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public final class GoldenDupes extends JavaPlugin {
@@ -15,8 +19,22 @@ public final class GoldenDupes extends JavaPlugin {
     public void onEnable() {
 
         //bStats metrics
-        new Metrics(this, 11145);
+        Metrics metrics = new Metrics(this, 11145);
+        metrics.addCustomChart(new SimpleBarChart("Server Sizes",
+                () -> {
+                    Map<String, Integer> map = new HashMap<>();
+                    int playercount = getServer().getOnlinePlayers().size();
 
+                    int[] cats = {0,0,5,10,20,50,75,100};
+                    for(int i = 1; i < cats.length; i++) {
+                        if(playercount <= cats[i]) {
+                            map.put((cats[i - 1] + 1) + "-" + (cats[i]), 1);
+                            return map;
+                        }
+                    }
+                    map.put("101+", 1);
+                    return map;
+                }));
 
         // fixing up config if it doesn't have some particular settings
         ConfigAutofill.autofill(this);
