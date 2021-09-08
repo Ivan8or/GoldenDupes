e228b1fcb6d80bce53e42561cf477aa30fd6eaef
Ivan8or <ivanthegreat825@gmail.com>
2021-09-08 10:36:56 -0400

    fixed bStats to show counts for servers of various sizes

--- src/main/java/online/umbcraft/libraries/GoldenDupes.java
+++ src/main/java/online/umbcraft/libraries/GoldenDupes.java
@@ -7,6 +7,7 @@ import online.umbcraft.libraries.dupes.DonkeyDupe;
 import online.umbcraft.libraries.dupes.NetherPortalDupe;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimpleBarChart;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -21,33 +22,49 @@ public final class GoldenDupes extends JavaPlugin {
         //bStats metrics
         Metrics metrics = new Metrics(this, 11145);
 
-        metrics.addCustomChart(new SimpleBarChart("Server Size",
-
+        // currently does not exist - placeholder
+        metrics.addCustomChart(new SimpleBarChart("server_sizes_bar",
                 () -> {
                     Map<String, Integer> map = new HashMap<>();
                     int playercount = getServer().getOnlinePlayers().size();
 
-                    int[] cats = {0, 0, 5, 10, 20, 50, 75, 100};
-                    int index = -1;
-                    for (int i = 1; i < cats.length; i++) {
-                        String barName = (cats[i-1] + 1) + "-" + (cats[i]);
-                        if (playercount > cats[i-1] && playercount <= cats[i]) {
-                            map.put(barName, 1);
-                            index = i;
+                    String[] categories = {"0","1","2-4","5-10","11-20","21-30","31-50","51-75","76-100","101+"};
+                    int[] limits =         {0,1,2,5,11,21,31,51,76,100};
+
+                    for(String cat: categories)
+                        map.put(cat,0);
+
+                    for(int i = 0; i < categories.length; i++)
+                        if(playercount <= limits[i]) {
+                            map.put(categories[i], 1);
+                            System.out.println("getting bStats metric for server size bar - returning "+categories[i]);
+                            return map;
                         }
-                        else
-                            map.put(barName, 0);
-                    }
 
-                    String catchAllCat = (cats[cats.length - 1] + 1) + "+";
-                    map.put(catchAllCat, (index == -1) ? 1 : 0);
+                    map.put(categories[categories.length-1],1);
+                    System.out.println("getting bStats metric for server size bar - returning 100+");
                     return map;
-
                 }));
+
+
+        metrics.addCustomChart(new SimplePie("server_sizes_pie",
+                () -> {
+                    int playercount = getServer().getOnlinePlayers().size();
+
+                    String[] categories = {"0","1","2-4","5-10","11-20","21-30","31-50","51-75","76-100","101+"};
+                    int[] limits =         {0,1,2,5,11,21,31,51,76,100};
+
+                    for(int i = 0; i < categories.length; i++)
+                        if(playercount <= limits[i]) {
+                            System.out.println("getting bStats metric for server size pie - returning "+categories[i]);
+                            return categories[i];
+                        }
+                    return categories[categories.length-1];
+                }));
+
 
         // fixing up config if it doesn't have some particular settings
         ConfigAutofill.autofill(this);
-
 
         // starts autocraft dupe handler if the dupe is enabled
         if (getConfig().getBoolean(ConfigPath.AUTOCRAFT_DO.path())) {
@@ -66,10 +83,6 @@ public final class GoldenDupes extends JavaPlugin {
             getServer().getPluginManager().registerEvents(
                     new NetherPortalDupe(this), this);
         }
-    }
-
-    public void onDisable() {
-        getServer().getScheduler().cancelTasks(this);
     }
 
 }
