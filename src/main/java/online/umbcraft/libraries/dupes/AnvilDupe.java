68cf769cd657e79531a58d894fde596bfe9afd9b
Ivan8or <ivanthegreat825@gmail.com>
2023-12-16 00:09:08 -0500

    added extra checks for block underneath the anvil

--- src/main/java/online/umbcraft/libraries/dupes/AnvilDupe.java
+++ src/main/java/online/umbcraft/libraries/dupes/AnvilDupe.java
@@ -3,15 +3,19 @@ package online.umbcraft.libraries.dupes;
 import online.umbcraft.libraries.GoldenDupes;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import java.util.*;
 
@@ -41,6 +45,7 @@ public class AnvilDupe extends Dupe implements Listener {
             return;
 
         Location l = e.getClickedInventory().getLocation();
+        Location blockBelow = l.add(new Vector(0,-1,0));
         ItemStack toDupe = e.getCurrentItem();
         Player p = (Player) e.getWhoClicked();
 
@@ -52,12 +57,15 @@ public class AnvilDupe extends Dupe implements Listener {
         if(l.getBlock().getType() != Material.DAMAGED_ANVIL)
             return;
 
+        // preventing the anvil or the block beneath the anvil from being broken / pushed
         anvilsInUse.add(l);
+        anvilsInUse.add(blockBelow);
 
         // check if anvil was destroyed by next tick
         plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () ->
         {
             anvilsInUse.remove(l);
+            anvilsInUse.remove(blockBelow);
 
             if (l.getBlock().getType() == Material.AIR && toDupe != null) {
                 Item dropped = p.getWorld().dropItem(p.getLocation(), dupe(toDupe, toDupe.getAmount()));
@@ -68,11 +76,40 @@ public class AnvilDupe extends Dupe implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onBreakInUseAnvil(final BlockBreakEvent e) {
-        if(e.getBlock().getType() != Material.DAMAGED_ANVIL)
+        if(anvilsInUse.isEmpty())
             return;
+
+        Location l = e.getBlock().getLocation();
 
         if(anvilsInUse.contains(e.getBlock().getLocation())) {
             e.setCancelled(true);
         }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPistonPush(final BlockPistonExtendEvent e) {
+        if(!isPistoningSafe(e.getBlocks()))
+            e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPistonPull(final BlockPistonRetractEvent e) {
+        if(!isPistoningSafe(e.getBlocks()))
+            e.setCancelled(true);
+    }
+
+    private boolean isPistoningSafe(List<Block> blocks) {
+        if(anvilsInUse.isEmpty())
+            return true;
+
+        for(Location anvilBlock : anvilsInUse) {
+            boolean involvesAnvil = blocks.stream()
+                    .map(Block::getLocation)
+                    .anyMatch((l) -> l.equals(anvilBlock));
+
+            if(involvesAnvil)
+                return false;
+        }
+        return true;
     }
 }
